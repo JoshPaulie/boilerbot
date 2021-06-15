4ef1ebbe0b19b2a6334ef2894b4dccd421703861
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2021-06-15 17:44:14 -0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime
+
+import discord
+from discord.ext import commands
+from dotenv import find_dotenv, load_dotenv
+
+from bot_config import BotConfig
+from bot_init_functions import collect_cogs, load_cogs
+
+intents = discord.Intents(messages=True, guilds=True, reactions=True)
+bot = commands.Bot(
+    command_prefix=BotConfig.prefix,
+    case_insensitive=True,
+    intents=intents,
+)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is ready!")
+
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+
+load_dotenv(find_dotenv())
+collect_cogs()
+load_cogs(bot)
+bot.run(os.getenv("DISCORD"))
---
+++ bot_init_functions.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+def collect_cogs():
+    files = Path("cogs").rglob("*.py")
+    for file in files:
+        if "__init__" not in file.name:
+            yield file.as_posix()[:-3].replace("/", ".")
+
+
+def load_cogs(bot):
+    for cog in collect_cogs():
+        try:
+            bot.load_extension(cog)
+        except Exception as e:
+            print(f"Failed to load cog {cog}\n{e}")
